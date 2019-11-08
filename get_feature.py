854fee772b631b7742edb3c4913000f57d955432
Yuzuru <33157737+YuzuruSakura@users.noreply.github.com>
2019-11-08 01:20:32 -0500

    Update get_feature.py

--- get_feature.py
+++ get_feature.py
@@ -7,24 +7,20 @@ import numpy as np
 
 from word_emb import emb_size, word2id, id2word, emb, word2count, vocab_size, SOS_token, EOS_token, PAD_token, UNK_token
 
-poem_type = 'poem7' # 需要修改
-hard_lv = True # 强lv
-
+with open('resource/word_dict.json', 'r', encoding='utf-8') as f1:
+    word_dict = json.load(f1)
+    
 forbidden_words = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '千', '百', '万',
                    '艇', '些', '的',
                    'START', 'END', '/', 'START1', 'END1', '-', 'UNK'] # END1
 # forbidden_id = [word2id[word] for word in forbidden_words]
+
 yun_sen = [2,4]
 
-lv_list = []
-sen_len = 0
-
-if poem_type == 'poem5':
-    sen_len = 5
-    lv_list = [['z', 'z', 'p', 'p', 'z'], ['p', 'p', 'z', 'z', 'p'], 
-                ['p', 'p', 'p', 'z', 'z'], ['z', 'z', 'z', 'p', 'p']]
-    
-elif poem_type == 'poem7':
+use_mode = False
+poem_type = 'poem7' # 需要修改
+hard_lv = True # 强lv
+if poem_type == 'poem7':
     sen_len = 7
     if hard_lv:
         lv_list = [['p', 'p', 'z', 'z', 'p', 'p', 'z'], ['z', 'z', 'p', 'p', 'z', 'z', 'p'],
@@ -32,9 +28,11 @@ elif poem_type == 'poem7':
     else:
         lv_list = [['0', 'p', '0', 'z', '0', 'p', '0'], ['0', 'z', '0', 'p', '0', 'z', 'p'],
                     ['0', 'z', '0', 'p', '0', 'z', '0'], ['0', 'p', '0', 'z', '0', 'p', 'p']]
+else:
+    sen_len = 5
+    lv_list = [['z', 'z', 'p', 'p', 'z'], ['p', 'p', 'z', 'z', 'p'], 
+                ['p', 'p', 'p', 'z', 'z'], ['z', 'z', 'z', 'p', 'p']]
 
-with open('resource/word_dict.json', 'r', encoding='utf-8') as f1:
-    word_dict = json.load(f1)
 
 all_lv = []
 for i in range(vocab_size):
@@ -55,17 +53,29 @@ for i in range(vocab_size):
         word_yun = ''
     all_yun.append(word_yun)
 all_yun = np.array(all_yun)
- 
-def get_feature(decoded_words, sen_num, w_num, batch_size):
+
+oov = 0
+def get_feature(decoded_words, sen_num, w_num, batch_size, target):    
     feature1 = []
     feature2 = []
     for i in range(batch_size):
-        target_lv = lv_list[sen_num][w_num]
+        # feature1: lv
+        if use_mode:
+            target_lv = lv_list[sen_num][w_num]
+        else:
+            target_word = id2word[str(target[i].item())]
+            if target_word in word_dict.keys():
+                target_lv = word_dict[target_word]['pz']
+            else:
+                target_lv = '0' # 如何让它
+                # oov += 1
+            
         target_lv = [target_lv] * vocab_size
         target_lv = np.array(target_lv)
         feature1_batch = np.where(all_lv == target_lv, 1.0, 0.0)
         feature1.append(feature1_batch)
 
+        # feature2: yun
         decoded_words_batch = decoded_words[i]
         if sen_num+1 in yun_sen[1:] and w_num+1 == sen_len:
             yun_word = decoded_words_batch[yun_sen[0] * (sen_len+1) - 2]  # 第一个押韵句子的最后一个字
